b4f4e2a1a62c96769c15dada650a93974b4af7cd
Peshchter <leniviy.putnik@gmail.com>
2021-05-14 00:26:32 +0400

    feat: add endpoints for PUT POST and DELETE methods

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -8,4 +8,28 @@ router.route('/').get(async (req, res) => {
   res.json(users.map(User.toResponse));
 });
 
+router.route('/').post(async (req, res) => {
+  const user = await usersService.save({name : req.body.name, password: req.body.password, login: req.body.login});
+  res.status(201).json(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = await usersService.update(req.params.id, req.body);
+  res.status(200).json(User.toResponse(user));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await usersService.remove(req.params.id);
+  res.status(200).send();
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.getById(req.params.id);
+  if(user){
+    res.status(200).json(User.toResponse(user))
+  } else {
+    res.status(404).json({ message: "user not found"});
+  }
+});
+
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,10 @@
 const usersRepo = require('./user.memory.repository');
 
-const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getAll = () => usersRepo.getAll();
+const getById = (id) => usersRepo.getById(id);
+const save = (user) => usersRepo.save(user);
+const remove = (id) => usersRepo.remove(id);
+const update = (id, params) => usersRepo.update(id, params);
+
+module.exports = { getAll, getById, save, remove, update };
