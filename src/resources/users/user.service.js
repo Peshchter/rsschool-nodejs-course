27d6094bcaf336c1c9ced686dcc7664a8d27da82
Peshchter <leniviy.putnik@gmail.com>
2021-05-14 11:17:42 +0400

    fix: fix error when on deleting user not deleting his id from task

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,10 +1,14 @@
 const usersRepo = require('./user.memory.repository');
+const tasksService  =require('../tasks/task.service');
 
 
 const getAll = () => usersRepo.getAll();
 const getById = (id) => usersRepo.getById(id);
 const save = (user) => usersRepo.save(user);
-const remove = (id) => usersRepo.remove(id);
+const remove = (id) => {
+    usersRepo.remove(id);
+    tasksService.removeUserId(id);
+}
 const update = (id, params) => usersRepo.update(id, params);
 
 module.exports = { getAll, getById, save, remove, update };
