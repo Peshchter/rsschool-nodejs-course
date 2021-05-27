c0389d8ade7b287fb37bce07a51e68d52c96c938
Peshchter <leniviy.putnik@gmail.com>
2021-05-27 15:11:57 +0400

    add: JSDoc to User model and Repo

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,24 +1,57 @@
+/**
+ * @namespace UserRepo
+ */
 const User = require('./user.model');
 
+/**
+ * Array of users
+ * @type User[]
+ * @memberof UserRepo
+ */
 let users = [new User({name : "Init_user", login : 'init', password : 'pass'})];
 
+/**
+ * get all users from database
+ * @memberof UserRepo
+ * @returns {User[]}
+ */
 const getAll = async () => users;
 
+/**
+ * get user by its ID
+ * @param {number} id - ID number of user
+ * @returns {User}
+ */
 const getById = async (id) => {
     const list = users.filter((user) => id === user.id);
     return list.length ? list[0] : null;
 }
 
+/**
+ * Creates the User and saves to DB
+ * @param {...User} params - Object with reqiured fields for creating User
+ * @returns {User}
+ */
 const save = async (params) => {
     const user = new User(params);
     users.push(user);
     return user;
 };
 
+/**
+ * removes the User from DB by his ID
+ * @param {number} id - ID of User for delete
+ */
 const remove = (id) => {
     users = users.filter((user) => user.id !== id);
 };
 
+/**
+ * update User fields in DB
+ * @param {number} id - ID of User to update
+ * @param {Object<User>} body - Object with updating fields 
+ * @returns {User} - Updated User
+ */
 const update = (id, body) => {
     const position = users.map( (element) => element.id ).indexOf(id);
     if (body.name) {
@@ -33,4 +66,7 @@ const update = (id, body) => {
     return users[position];
 };
 
+/**
+ * Exports reqiured functions
+ */
 module.exports = { getAll, getById, save, remove, update };
--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,18 +1,42 @@
 const uuid = require('uuid');
 
+/**
+ * @class User
+ */
 class User {
+  /**
+   * @constructor
+   * @param {Object} [body] - request body containing required fields
+   */
   constructor({
     id = uuid.v4(),
     name = 'USER',
     login = 'user',
     password = 'P@55w0rd'
   } = {}) {
+    /**
+    * @property {number} id
+    */
     this.id = id;
+    /**
+    * @property {string} name
+    */
     this.name = name;
+    /**
+    * @property {string} login
+    */
     this.login = login;
+    /**
+    * @property {string} password
+    */
     this.password = password;
   }
 
+/**
+ * prepare model to response
+ * @param {User} user - User to response
+ * @returns {...User} - fields of model to represent
+ */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
