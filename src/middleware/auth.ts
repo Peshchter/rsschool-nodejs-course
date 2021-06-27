ce4389b2ee11e7bd69f63b2308edc23ba0f73be8
Peshchter <leniviy.putnik@gmail.com>
2021-06-27 22:42:44 +0400

    feat: auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,24 @@
-import {Request, Response, NextFunction} from 'express'; 
+import { Request, Response, NextFunction } from 'express';
+import { config } from '../common/config';
+import * as jwt from 'jsonwebtoken';
 
-export function auth (req: Request, _res: Response, next:NextFunction) {
-    console.log(req.header);
-    next();
+export function checkAuth(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.header('Authorization');
+    if (authHeader === undefined) {
+        res.status(401).send('Unauthorized!');
+    } else {
+        const [type, tokenString] = authHeader.split(' ');
+        if (type !== 'Bearer' || tokenString === 'undefined' || tokenString === undefined ){
+            res.status(401).send('Wrong auth type!');
+        }else{
+            try {
+                jwt.verify(tokenString, config.JWT_SECRET_KEY);
+            }catch(e){
+                res.status(401).send('Unauthorized!');
+            }
+            return next();
+        }   
+    }
 };
 
 
