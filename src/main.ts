057f1582bfb89b193549c9e10632f5e0f49abba8
Peshchter <leniviy.putnik@gmail.com>
2021-07-04 20:43:20 +0400

    add: fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
 import { config } from './common/config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrapE() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn'],
+  });
   await app.listen(config.PORT);
 }
-bootstrap();
+
+async function bootstrapF() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true })
+  );
+  await app.listen(config.PORT, '0.0.0.0');
+}
+
+config.USE_FASTIFY ? bootstrapF() : bootstrapE();
+
