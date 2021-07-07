b13a35b7a1467e4a0dc2d7fd054dcd56a491663a
Peshchter <leniviy.putnik@gmail.com>
2021-07-07 16:27:18 +0400

    feat: artillery test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app/app.module';
 import { FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
 import { config } from './common/config';
 import { MyLogger } from './middleware/logger';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   let app;
@@ -11,32 +10,13 @@ async function bootstrap() {
   if (config.USE_FASTIFY === true){
     app = await NestFactory.create<NestFastifyApplication>(
       AppModule,
-      new FastifyAdapter({ logger: true })
+      new FastifyAdapter()
     );
   }else {
-    app = await NestFactory.create(AppModule, {
-      logger: ['log', 'warn', 'error']
-    });
+    app = await NestFactory.create(AppModule);
   }
     app.useGlobalInterceptors(new MyLogger());
   await app.listen(config.PORT, '0.0.0.0');
 }
-
-process.on('unhandledRejection', () => {
-  Logger.error(`Error: Request failed!`);
-});
-
-process.on('uncaughtException', (err) => {
-  Logger.error(`At ${new Date().toLocaleTimeString("ru", {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      weekday: 'long',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-  })} occurred an Internal Server Error with message ${err.message}! Terminating ...`);
-  process.exit(1);
-});
 
 bootstrap();
